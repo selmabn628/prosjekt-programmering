e04b7c583ddaf241306043ff921ee1b91cc8890b
[Selma Nissen] <[selmanissen05@gmail.com]>
2025-05-21 12:09:04 +0200

    la til docstring på hovedkoden for forkalring

--- data/laks_api_for_unittest.py
+++ data/laks_api_for_unittest.py
@@ -1,3 +1,7 @@
+"""
+Koden henter laksedata fra SSB og gjør dataen om til en tabell for alle verdiene som brukes videre i prosjektet ved hjelp av Pandas.
+"""
+
 import requests
 import pandas as pd
 from itertools import product
@@ -21,19 +25,19 @@ def process_data(data):
     """Prosesserer JSON-data og returnerer en formatert Pandas DataFrame."""
     dimension_names = list(data["dimension"].keys())
     values = data["value"]
-    
-    # Opprette en liste av kombinasjoner av dimensjoner
+   
+
     dimensions = [list(data["dimension"][dim]["category"]["label"].values()) for dim in dimension_names]
     all_combinations = list(product(*dimensions))
     
-    # Lage en DataFrame med riktige dimensjoner
+
     df = pd.DataFrame(all_combinations, columns=dimension_names)
-    df["Value"] = values  # Knytte verdiene til riktige rader
+    df["Value"] = values  
     
-    # Pivotere data for å få ønsket struktur
+
     df_pivot = df.pivot_table(index="Tid", columns=["VareGrupper2", "ContentsCode"], values="Value", aggfunc="sum").reset_index()
     
-    # Gi kolonnene mer forståelige navn med riktig rekkefølge
+    
     df_pivot.columns = ["År og ukenr.", "Fersk laks - Kilospris", "Fersk laks - Vekt (tonn)", "Frosset laks - Kilospris", "Frosset laks - Vekt (tonn)"]
     
     return df_pivot
